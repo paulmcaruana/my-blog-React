bab3c10fd6a7afe73d8e8107dd81b099695b44ce
Paul Caruana <paul@beambient.co.uk>
2020-08-18 09:31:47 +0100

    first commit

---
+++ src/build/precache-manifest.b8859ad7005bb48a4dd40f908ab8fded.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b6fdd16767fff9cc5ee775fc5cb4577",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f0fce4ad1d030f5b1457",
+    "url": "/static/css/main.fe6824ef.chunk.css"
+  },
+  {
+    "revision": "f7742bd7ca531cfad47f",
+    "url": "/static/js/2.b09d3ca3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b09d3ca3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0fce4ad1d030f5b1457",
+    "url": "/static/js/main.252e26cf.chunk.js"
+  },
+  {
+    "revision": "8931283fa7532b66ee7f",
+    "url": "/static/js/runtime-main.fca178c7.js"
+  }
+]);
